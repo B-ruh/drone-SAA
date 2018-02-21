29c2e54fb0a3646da0341bca44ffcdd97baa46a0
Thomas Watson <twatson52@icloud.com>
2018-02-21 09:00:17 -0600

    clean up some coactor stuff and write more comments

--- coactor.py
+++ coactor.py
@@ -29,41 +29,56 @@ class CoActor(Actor):
     def receiveMessage(self, msg, sender):
         # we've got a message
 
+        # did something handle it?
         was_handled = False
 
         # keep list of coros made from this message so they don't see
         # the message twice
         new_coros = []
 
+        # helper function to send a new value into a coroutine
+        # then deal with the thing it wants to await on
         def send_coro(coro, obj):
             try:
                 wobj = coro.send(obj)
             except StopIteration:
-                wobj = None
+                # coro is completely finished
+                return
             except:
                 # oh heck
                 traceback.print_exc()
                 return
-            if wobj is not None:
-                if isinstance(wobj, Future):
-                    wobj._waiters.append(coro)
-                else:
-                    new_coros.append((wobj, coro))
 
-        # 0th step: execute the call soon callbacks
+            if isinstance(wobj, Future):
+                # it wants to wait on a future
+                # add it to the future's waiters
+                wobj._waiters.append(coro)
+            else:
+                # something else, handle that once all coros
+                # are processed
+                new_coros.append((wobj, coro))
+
         if isinstance(msg, CoActor.CallSoon):
             self._call_soon_sent = False
             was_handled = True
 
+        # 0th step: execute the call soon callbacks
         if len(self._call_soon) > 0:
             cs = self._call_soon
             self._call_soon = []
             for coro in cs:
                 if hasattr(coro, 'send'):
+                    # it's a coroutine object that needs a new value
+                    # send a new one into it
                     send_coro(coro, None)
                 elif not inspect.iscoroutinefunction(coro):
+                    # it's a boring old function
+                    # just call it
                     coro()
                 else:
+                    # it's a coroutine function, which needs to be called
+                    # to produce a coroutine object, so do that then send
+                    # a new value into it
                     send_coro(coro(), None)
 
         # first step: see if any callbacks need to be processed
@@ -72,15 +87,16 @@ class CoActor(Actor):
                 was_handled = True
                 # call the callback and get a coroutine
                 coro = cb(msg, sender)
-                # now send it to get it running
+                # now send to it to get it running
                 send_coro(coro, None)
 
         # second step: see if any coros were waiting on that message
-        if type(msg) in self._pending_coros:
-            coros = self._pending_coros[type(msg)]
-            del self._pending_coros[type(msg)]
+        mtype = type(msg)
+        if mtype in self._pending_coros:
+            was_handled = True
+            coros = self._pending_coros[mtype]
+            del self._pending_coros[mtype]
             for coro in coros:
-                was_handled = True
                 # send the new message into the coroutine
                 send_coro(coro, (msg, sender))
         
@@ -105,12 +121,12 @@ class CoActor(Actor):
 
     # sleep for a specified number of seconds asynchronously
     async def sleep(self, seconds):
-        # instantiate new message waiter
+        # instantiate new message waiter that waits for a WakeupMessage
         w = CoActor.MessageWaiter(WakeupMessage)
         # schedule a wakeup to be sent in the specified seconds
         self.wakeupAfter(timedelta(seconds=seconds), payload=w)
         while True:
-            # wait for a new wakeupmessage
+            # use the message waiter to wait for a WakeupMessage
             msg, sender = await w
             # make sure it's for this wakeup
             if msg.payload == w: break
@@ -133,8 +149,10 @@ class CoActor(Actor):
     def call_soon(self, coro):
         # append it to call soon list
         self._call_soon.append(coro)
-        # and send a call soon message to trig ger the call
+        # and send a call soon message to trigger the call,
         # (assuming we haven't sent one since the last time)
+        # as call soon routines are only called when any message
+        # is received
         if not self._call_soon_sent:
             self._call_soon_sent = True
             self.send(self.myAddress, CoActor.CallSoon())
