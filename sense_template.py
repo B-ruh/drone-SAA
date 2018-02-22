94320160c7706fc7e8019e67c8955743977588f6
Thomas Watson <twatson52@icloud.com>
2018-02-22 12:42:49 -0600

    wrap sense template to hide thespian shenanigans

--- sense_template.py
+++ sense_template.py
@@ -1,18 +1,7 @@
-from thespian.actors import *
-import time
 from nav import DroneInDanger
+import time
 
-import sys
-
-class NavDummy(Actor):
-    def receiveMessage(self, msg, sender):
-        if isinstance(msg, DroneInDanger):
-            print("Danger status: {}".format(msg.danger))
-
-asys = None
-nav = None
-
-def main():
+def main(asys, nav):
     asys.tell(nav, DroneInDanger(False))
 
     print("press ENTER to see an obstacle")
@@ -21,29 +10,3 @@ def main():
     time.sleep(5)
     asys.tell(nav, DroneInDanger(False))
 
-if __name__ == "__main__":
-    try:
-        try:
-            dummy = sys.argv[1] != "for_real"
-        except:
-            dummy = True
-
-        if dummy:
-            asys = ActorSystem(systemBase="multiprocTCPBase",
-                capabilities={"Admin Port": 1999})
-        else:
-            asys = ActorSystem(systemBase="multiprocTCPBase", 
-                capabilities={"Convention Address.IPv4": ('localhost', 1900),
-                                "Admin Port": 1901, 
-                                "nav_system": False})
-
-        nav = None
-
-        if dummy:
-            nav = asys.createActor(NavDummy)
-        else:
-            nav = asys.createActor('nav.Navigation', globalName="Navigation")
-
-        main()
-    finally:
-        asys.shutdown()
